3dd6a423878b6e29bb881d849d1300ba6fe9d766
av <a@v>
2026-09-11 10:25:33 +0530

    competed all cover art for Lord Vishwakarma

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
  * Provides offline reading capabilities and fast cached assets.
  */
 
-const CACHE_NAME = 'gyankosh-v2';
+const CACHE_NAME = 'gyankosh-v3';
 const BASE = '/gyankosh';
 
 // Core assets to pre-cache on install
